50718dbf8b07c9e4c1c26b5148af3d4a28a5a23f
theJuanon <jhernandezangulo@gmail.com>
2021-07-25 17:36:55 -0600

    agrega diseño a homepage y navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        <img src="./assets/icon.png" alt="" />
+      </router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+  background-color: #3eecac;
+  background-image: linear-gradient(315deg, #3eecac 0%, #ee74e1 100%);
+  background-repeat: no-repeat;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,7 +26,25 @@
 }
 
 #nav {
-  padding: 30px;
+  position: fixed;
+  top: 0;
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fa8bff;
+  background-image: linear-gradient(
+    135deg,
+    #fa8bff 0%,
+    #2bd2ff 52%,
+    #2bff88 90%
+  );
+  border-bottom: 1px #fa8bff solid;
+}
+
+#nav img {
+  width: 70px;
 }
 
 #nav a {
@@ -28,5 +54,13 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#about {
+  background: #fbf5f3;
+  border: 0px black solid;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 5px;
 }
 </style>
