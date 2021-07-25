693806fef09b450dba18b926e38f67a735555257
theJuanon <56099545+theJuanon@users.noreply.github.com>
2021-07-25 17:36:55 -0600

    agrega diseño a homepage y navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png" alt="">
+    <div id="banner">
+      <img src="../assets/logo.png" alt="" />
+    </div>
     <div v-if="loading">Cargando...</div>
     <div class="cards">
       <Card
@@ -17,31 +19,40 @@
 </template>
 
 <script>
-import {mapState, mapActions} from 'vuex'
-import Card from '@/components/Card.vue'
+import { mapState, mapActions } from "vuex";
+import Card from "@/components/Card.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Card
+    Card,
   },
   computed: {
-    ...mapState(['movies', 'loading'])
+    ...mapState(["movies", "loading"]),
   },
   methods: {
-    ...mapActions(['getMovies'])
+    ...mapActions(["getMovies"]),
   },
   mounted() {
     this.getMovies();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  .cards {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
-    margin: 10px;
-  }
+.cards {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  margin: 10px;
+}
+
+#banner {
+  margin-top: 70px;
+  width: 100%;
+}
+
+#banner img {
+  width: 95%;
+}
 </style>
