ade9451a64642bebb97e0bd4dc393ab6e6105d4c
theJuanon <jhernandezangulo@gmail.com>
2021-07-25 20:02:27 -0600

    la card es enlace

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="card">
-    <router-link class="link" :to="`/pelicula/detalle/${id}`">
-      <h4>
-        {{ titulo }}<span>({{ fecha }})</span>
+  <router-link class="link" :to="`/pelicula/detalle/${id}`">
+    <div class="card">
+      <h4 class="card__title">
+        {{ titulo }}<span class="card__title--date">({{ fecha }})</span>
       </h4>
-    </router-link>
-    <p class="card__director">Directed by: {{ director }}</p>
-    <p v-if="score > 80">Score: {{ score }}</p>
-    <p>id: {{ id }}</p>
-  </div>
+      <p class="card__director">Directed by: {{ director }}</p>
+      <p v-if="score > 80">Score: {{ score }}</p>
+      <p>id: {{ id }}</p>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-  name: "Tarjeta",
+  name: "Card",
   props: {
     id: {
       type: String,
@@ -40,28 +40,30 @@ export default {
 </script>
 
 <style scoped>
+.link {
+  text-decoration: none;
+}
 .card {
   border: 1px black solid;
   border-radius: 10px;
   transition: 0.5s;
   background-size: 200% auto;
-  background-color: #FF3cea;
+  background-color: #ff3cea;
   color: white;
 }
 
 .card:hover {
-  background-color: #3377FF;
-  color: #FFFFFF;
+  background-color: #3377ff;
+  color: #ffffff;
   text-decoration: none;
 }
 
-.link {
-  text-decoration: none;
-  color: #05F140;
+.card__title {
+  color: #05f140;
   font-size: 24px;
 }
 
-.link span {
+.card__title--date {
   color: #706979;
   font-weight: lighter;
   font-size: 16px;
