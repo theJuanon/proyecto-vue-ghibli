cc7a00fcde4ec94d5387db98c022fba9b10b31e7
theJuanon <jhernandezangulo@gmail.com>
2021-07-25 18:54:29 -0600

    agregado estilo a cards y detalles

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,60 +1,69 @@
 <template>
   <div class="card">
     <router-link class="link" :to="`/pelicula/detalle/${id}`">
-      <h4>{{titulo}}<span>({{fecha}})</span></h4>
+      <h4>
+        {{ titulo }}<span>({{ fecha }})</span>
+      </h4>
     </router-link>
-    <p class="card__director">Directed by: {{director}}</p>
-    <p
-      v-if="score > 80"
-    >
-      Score: {{score}}
-    </p>
-    <p>{{id}}</p>
+    <p class="card__director">Directed by: {{ director }}</p>
+    <p v-if="score > 80">Score: {{ score }}</p>
+    <p>id: {{ id }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Tarjeta",
-    props: {
-      id: {
-        type: String,
-        required: true
-      },
-      titulo: {
-        type: String,
-        required: true
-      },
-      director: {
-        type: String,
-        required: true
-      },
-      fecha: {
-        type: String,
-        required: true
-      },
-      score: {
-        type: String,
-        required: true
-      }
-    }
-  }
+export default {
+  name: "Tarjeta",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    titulo: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    fecha: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .card {
-    border: 1px black solid;
-    border-radius: 5px;
-  }
+.card {
+  border: 1px black solid;
+  border-radius: 10px;
+  transition: 0.5s;
+  background-size: 200% auto;
+  background-color: #FF3cea;
+  color: white;
+}
 
-  .link {
-    text-decoration: none;
-    font-size: 24px;
-  }
+.card:hover {
+  background-color: #3377FF;
+  color: #FFFFFF;
+  text-decoration: none;
+}
 
-  .link span {
-    color: gray;
-    font-weight: lighter;
-    font-size: 16px;
-  }
+.link {
+  text-decoration: none;
+  color: #05F140;
+  font-size: 24px;
+}
+
+.link span {
+  color: #706979;
+  font-weight: lighter;
+  font-size: 16px;
+}
 </style>
