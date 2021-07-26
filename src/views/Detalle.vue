989f51d5bd5a8d0a4bd0c58af985be5afdda1f9c
theJuanon <56099545+theJuanon@users.noreply.github.com>
2021-07-25 19:47:56 -0600

    corregido diseño en detalle

--- src/views/Detalle.vue
+++ src/views/Detalle.vue
@@ -2,15 +2,16 @@
   <div class="details">
     <h1>{{ movie.title }}</h1>
     <p id="original">Original Title: {{ movie.original_title_romanised }}</p>
-    <p>Director: {{ movie.director }}</p>
-    <p>Release Date: {{ movie.release_date }}</p>
-    <p id="sinopsis">Sinposis: {{ movie.description }}</p>
-    <p>Duration: {{ movie.running_time }} minutes.</p>
+    <p><span class="prop">Director:</span> {{ movie.director }}</p>
+    <p><span class="prop">Release Date:</span> {{ movie.release_date }}</p>
+    <h2>Sinopsis:</h2>
+    <p id="sinopsis">{{ movie.description }}</p>
+    <p><span class="prop">Duration:</span> {{ movie.running_time }} minutes.</p>
     <p v-if="movie.rt_score > 80" class="score--green">
       Score: {{ movie.rt_score }}
     </p>
     <p v-else class="score--yellow">Score: {{ movie.rt_score }}</p>
-    <p>id: {{ movie.id }}</p>
+    <p id="id"><span class="prop">id:</span> {{ movie.id }}</p>
   </div>
 </template>
 
@@ -33,7 +34,7 @@ export default {
 <style scoped>
 .details {
   margin-top: 71px;
-  padding: 10px 300px 0 300px;
+  padding: 10px 15% 0 15%;
   font-size: 18px;
 }
 
@@ -43,6 +44,11 @@ h1 {
   margin-bottom: 0;
 }
 
+h2 {
+  color: #0087B8;
+  margin-bottom: 0px;
+}
+
 #original {
   margin-top: 0;
   font-size: 22px;
@@ -50,7 +56,12 @@ h1 {
   color: #0087B8;
 }
 
+.prop {
+  font-weight: bold;
+}
+
 #sinopsis {
+  margin-top: 0px;
   font-size: 20px;
 }
 
@@ -60,5 +71,9 @@ h1 {
 
 .score--yellow {
   color: yellow;
+}
+
+#id {
+  margin-top: 40px;
 }
 </style>
